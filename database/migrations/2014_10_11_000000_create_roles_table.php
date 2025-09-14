836f4cb1c39ad7415f95e67fef57ef45eef7c315
Justin Micah <jmika734@gmail.com>
2025-09-14 12:04:15 +0100

    re-ordonnancement des migrations

--- database/migrations/2014_10_11_000000_create_roles_table.php
+++ database/migrations/2014_10_11_000000_create_roles_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
 	 */
 	public function up(): void
 	{
-		Schema::create('approvisionnements', function (Blueprint $table) {
+		Schema::create('roles', function (Blueprint $table) {
 			$table->id();
-			$table->date('date');
-			$table->string('fournisseur')->nullable();
-			$table->foreignId('utilisateur_id')->constrained('users')->onDelete('cascade');
+			$table->string('nom');
 			$table->timestamps();
 		});
 	}
@@ -25,6 +23,6 @@ return new class extends Migration
 	 */
 	public function down(): void
 	{
-		Schema::dropIfExists('approvisionnements');
+		Schema::dropIfExists('roles');
 	}
 };
